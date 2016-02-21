acfe2be1da73210ceabcac8bb441688e23beb314
Andres Vahter <andres.vahter@gmail.com>
2016-02-21 16:36:52 +0200

    do not deny trivial_casts and trivial_numeric_casts

--- src/lib.rs
+++ src/lib.rs
@@ -22,8 +22,7 @@
  * SOFTWARE.
  */
 
-#![deny(missing_debug_implementations, trivial_casts, trivial_numeric_casts,
-        unstable_features, unused_import_braces, unused_qualifications)]
+#![deny(missing_debug_implementations, unstable_features, unused_import_braces, unused_qualifications)]
 
 
 #[macro_use]
